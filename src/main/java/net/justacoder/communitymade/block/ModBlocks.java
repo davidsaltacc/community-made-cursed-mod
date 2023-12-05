20ad53018def607254a496d04cb9a9a1f43a2cb3
davidsaltacc <d.lumpi@outlook.de>
2023-12-05 09:14:59 +0100

    fixes

--- src/main/java/net/justacoder/communitymade/block/ModBlocks.java
+++ src/main/java/net/justacoder/communitymade/block/ModBlocks.java
@@ -4,6 +4,8 @@ import net.justacoder.communitymade.Main;
 import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.MapColor;
 import net.minecraft.block.enums.Instrument;
+import net.minecraft.item.BlockItem;
+import net.minecraft.item.Item;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.sound.BlockSoundGroup;
@@ -15,6 +17,7 @@ public abstract class ModBlocks {
 
     public static void addBlocks() {
         Registry.register(Registries.BLOCK, new Identifier(Main.MOD_ID, "zombified_crafting_table"), ZCT_BLOCK);
+        Registry.register(Registries.ITEM, new Identifier(Main.MOD_ID, "zombified_crafting_table"), new BlockItem(ZCT_BLOCK, new Item.Settings()));
     }
 
 }
